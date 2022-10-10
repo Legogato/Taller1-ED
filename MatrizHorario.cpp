64785b693fd3668b7498d3505190b9a18737130d
Pablo <Pablo@DESKTOP-PSBQDHD>
2022-10-09 21:33:48 -0300

    si

--- MatrizHorario.cpp
+++ MatrizHorario.cpp
@@ -1,1 +1,10 @@
 #include "MatrizHorario.h"
+#include "NodoMatriz.h"
+
+MatrizHorario::MatrizHorario(int cols, int rows)
+{
+	this->cols = cols;
+	this->rows = rows;
+	this->aRow = new NodoMatriz(rows);
+	this->aCol = new NodoMatriz(cols);
+}
--- MatrizHorario.h
+++ MatrizHorario.h
@@ -6,14 +6,27 @@ class MatrizHorario
 private:
 
 	/// <summary>
-	/// matriz
+	/// filas matriz 
 	/// </summary>
-
 	NodoMatriz* aRow;
+	/// <summary>
+	/// columnas matriz
+	/// </summary>
 	NodoMatriz* aCol;
+	/// <summary>
+	/// cantidad de columnas
+	/// </summary>
 	int cols;
+	/// <summary>
+	/// cantidad de filas
+	/// </summary>
 	int rows;
 public:
+	/// <summary>
+	/// Constructor de la clase
+	/// </summary>
+	/// <param name="cols"></param>
+	/// <param name="rows"></param>
 	MatrizHorario(int cols, int rows);
 };
 
