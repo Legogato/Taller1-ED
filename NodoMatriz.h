29f27b28f545321a271fd38d2e4365c3eb1a16f5
PONSO03 <matias.araya04@alumnos.ucn.cl>
2022-10-09 22:26:59 -0300

    hola 2

--- NodoMatriz.h
+++ NodoMatriz.h
@@ -6,12 +6,66 @@ class NodoMatriz
 
 {
 private:
-	Asignatura* asignatura;
-	int row;
-	int col;
-	NodoMatriz* up;
-	NodoMatriz* left;
-public:
-	NodoMatriz(Asignatura* asignatura, int row, int col);
-};
 
+	/// <summary>
+	/// Asignatura almacenada en el nodo
+	/// </summary>
+
+	Asignatura* asignatura;
+
+	/// <summary>
+	/// Fila
+	/// </summary>
+
+	int row;
+
+	/// <summary>
+	/// Columna
+	/// </summary>
+
+	int col;
+
+	/// <summary>
+	/// Nodo de arriba
+	/// </summary>
+
+	NodoMatriz* up;
+
+	/// <summary>
+	/// Nodo de la izquierda
+	/// </summary>
+
+	NodoMatriz* left;
+
+public:
+
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="asignatura"></param>
+	/// <param name="row"></param>
+	/// <param name="col"></param>
+
+	NodoMatriz(Asignatura* asignatura, int row, int col);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+
+	Asignatura getAsignatura();
+	void setAsignatura(Asignatura asignatura);
+
+	int getRow();
+	void setRow(int row);
+
+	int getCol();
+	void setCol(int col);
+
+	NodoMatriz getUp();
+	void setUp(NodoMatriz up);
+
+	NodoMatriz getLeft();
+	void setLeft(NodoMatriz left);
+
+};
